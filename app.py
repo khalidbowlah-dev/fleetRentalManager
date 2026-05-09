af63b1b1149dc55db5af01ee6df81dde0afb1724
KHALED BOWLLAH MOUSSA <khalidbowlah@gmail.com>
2026-05-09 23:47:59 +0300

    [US1] Add truck registration form and routing logic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here' # Needed for sessions later
@@ -22,7 +22,21 @@ def init_db():
 def index():
     return "<h1>Fleet Manager is Running!</h1><p>The database is ready.</p>"
 
+@app.route('/add_truck', methods=['GET', 'POST'])
+def add_truck():
+    # If the user clicks the "Add Vehicle" button, this code runs:
+    if request.method == 'POST':
+        model = request.form['model']
+        daily_rate = request.form['daily_rate']
+
+        # We will add the raw SQL to save this to the database in the next step!
+        print(f"Success: Received {model} at ${daily_rate}/day")
+        return redirect(url_for('index'))
+
+    # If the user is just visiting the page, show them the HTML form:
+    return render_template('add_truck.html')
+
 if __name__ == '__main__':
     # Uncomment the line below the first time you run this to create the DB
-    init_db()
+    # init_db()
     app.run(debug=True)
