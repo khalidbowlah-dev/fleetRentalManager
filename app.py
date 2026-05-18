a21a21e5c02c23414001930a2b0ec983ebec0d48
KHALED BOWLLAH MOUSSA <khalidbowlah@gmail.com>
2026-05-18 20:02:26 +0300

    [US1] Display fleet inventory from database on homepage

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here' # Needed for sessions later
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-# This function will create the database using your schema.sql
 def init_db():
     with app.app_context():
         db = get_db_connection()
@@ -20,23 +19,31 @@ def init_db():
 
 @app.route('/')
 def index():
-    return "<h1>Fleet Manager is Running!</h1><p>The database is ready.</p>"
+    # Connect to the database and grab all trucks
+    db = get_db_connection()
+    trucks = db.execute('SELECT * FROM trucks').fetchall()
+    db.close()
+
+    # Send those trucks to your index.html file
+    return render_template('index.html', trucks=trucks)
 
 @app.route('/add_truck', methods=['GET', 'POST'])
 def add_truck():
-    # If the user clicks the "Add Vehicle" button, this code runs:
     if request.method == 'POST':
         model = request.form['model']
         daily_rate = request.form['daily_rate']
 
-        # We will add the raw SQL to save this to the database in the next step!
-        print(f"Success: Received {model} at ${daily_rate}/day")
+        # Connect to DB and INSERT the new truck
+        db = get_db_connection()
+        db.execute('INSERT INTO trucks (owner_id, model, daily_rate) VALUES (?, ?, ?)',
+                   (1, model, daily_rate))
+        db.commit()
+        db.close()
+
         return redirect(url_for('index'))
 
-    # If the user is just visiting the page, show them the HTML form:
     return render_template('add_truck.html')
 
 if __name__ == '__main__':
-    # Uncomment the line below the first time you run this to create the DB
     # init_db()
     app.run(debug=True)
